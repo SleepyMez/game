3d935e638b23fa487709f37e169a84bb95dd767a
rubyflarm <rblx15905@gmail.com>
2024-10-24 21:01:22 -0700

    oops quick day fix

--- public/lib/util.js
+++ public/lib/util.js
@@ -172,5 +172,5 @@ export const isHalloween = (() => {
     const month = now.getMonth() + 1;
     const day = now.getDate();
 
-    return month === 10 && day >= 3 || month === 11 && day <= 8;
+    return month === 10 && day >= 28 || month === 11 && day <= 8;
 })();
